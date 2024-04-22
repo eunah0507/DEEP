586d3e1e6f7a91a96001cfd916d57ef9c4028c61
eunah0507 <eunah0507@naver.com>
2024-04-23 00:07:19 +0900

    :sparkles:feat: Member-SocialLogin Setting 완료 (Google, Naver, Kakao)

---
+++ BE/deep/src/main/java/com/basic/deep/auth/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.basic.deep.auth.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public class ErrorResponse extends RuntimeException {
+
+    private int statusCode;
+
+    private HttpStatus httpStatus;
+
+    private String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        this.statusCode = status.value();
+        this.message = message;
+        this.httpStatus = status;
+    }
+
+}
