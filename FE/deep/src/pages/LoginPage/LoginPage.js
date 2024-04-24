81b65e99c6731486df6a885a59020c30a4737dcd
WHOOZ-23 <riahboa@naver.com>
2024-04-24 22:02:47 +0900

    :sparkles:feat: 로그인 화면 구현 완료

--- FE/deep/src/pages/LoginPage/LoginPage.js
+++ FE/deep/src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,99 @@
 import { Link } from "react-router-dom";
 import { LoginContainer } from "./LoginPage.styles";
-import logo from "../../assets/images/deep-logo-header.svg";
+import deepLogo from "../../assets/images/deep-logo-header.svg";
 import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import styled from "styled-components";
+import kakaoLogo from "../../assets/images/deep-kakao-logo.svg";
+import naverLogo from "../../assets/images/deep-naver-logo.svg";
+import googleLogo from "../../assets/images/deep-google-logo.svg";
+
+const KakaoLogin = styled(Button)`
+    margin-bottom: 15px;
+    background: #fae100;
+    color: #000;
+    font-weight: 500;
+
+    &:hover {
+        background: #fae100;
+    }
+
+    img {
+        height: 16px;
+        margin-right: 10px;
+    }
+`;
+
+const NaverLogin = styled(Button)`
+    margin-bottom: 15px;
+    background: #03c75a;
+
+    &:hover {
+        background: #03c75a;
+    }
+
+    img {
+        height: 16px;
+        margin-right: 10px;
+    }
+`;
+
+const GoogleLogin = styled(Button)`
+    font-weight: 500;
+
+    &:hover {
+        background: #fff;
+    }
+
+    img {
+        height: 16px;
+        margin-right: 10px;
+    }
+`;
 
 function LoginPage() {
     return (
         <LoginContainer>
-            <Link to="/" className="logo">
-                <img src={logo} alt="deep-logo" />
-            </Link>
-            <Input type="text" label="아이디" placeholder="아이디" />
+            <div className="logo">
+                <Link to="/">
+                    <img src={deepLogo} alt="deep-logo" />
+                </Link>
+            </div>
+            <div className="login">
+                <Input type="text" label="아이디" placeholder="아이디" />
+                <Input
+                    type="password"
+                    label="비밀번호"
+                    placeholder="비밀번호"
+                />
+                <Button mediumWidth>로그인</Button>
+            </div>
+            <div className="member">
+                <div className="find">
+                    <Link to="/find-id">아이디 찾기</Link>
+                    <Link to="/find-password">비밀번호 찾기</Link>
+                </div>
+                <div className="signUp">
+                    <Link to="/signup">회원가입</Link>
+                </div>
+            </div>
+            <div className="orLine">
+                <span className="line"></span>
+                <span className="or">또는</span>
+                <span className="line"></span>
+            </div>
+            <KakaoLogin mediumWidth>
+                <img src={kakaoLogo} alt="kakao-logo" />
+                <span>카카오로 로그인하기</span>
+            </KakaoLogin>
+            <NaverLogin mediumWidth>
+                <img src={naverLogo} alt="naver-logo" />
+                <span>네이버로 로그인하기</span>
+            </NaverLogin>
+            <GoogleLogin mediumWidth inverted>
+                <img src={googleLogo} alt="google-logo" />
+                <span>구글로 로그인하기</span>
+            </GoogleLogin>
         </LoginContainer>
     );
 }
--- FE/deep/src/pages/LoginPage/LoginPage.styles.js
+++ FE/deep/src/pages/LoginPage/LoginPage.styles.js
@@ -1,20 +1,71 @@
 import styled from "styled-components";
+import { palette } from "../../styles/palette";
 
 export const LoginContainer = styled.div`
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    display: flex;
-    justify-content: center;
     width: 400px;
 
+    /* logo */
     .logo {
-        display: inline-block;
         width: 100%;
+        margin-bottom: 50px;
+        text-align: center;
     }
 
-    img {
+    .logo img {
         width: 130px;
+    }
+
+    /* login */
+    .login {
+        margin-bottom: 20px;
+    }
+
+    /* member */
+    .member {
+        font-size: 1.4rem;
+    }
+
+    .find {
+        float: left;
+    }
+
+    .find > a {
+        margin-right: 10px;
+    }
+
+    .signUp {
+        float: right;
+    }
+
+    .signUp > a {
+        text-decoration: underline;
+        color: ${palette.blue[6]};
+        font-weight: 600;
+    }
+
+    .orLine {
+        float: left;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        margin: 30px 0 15px;
+    }
+
+    .orLine .line {
+        display: inline-block;
+        width: 175px;
+        height: 1px;
+        background: #ddd;
+    }
+
+    .orLine .or {
+        display: inline-block;
+        font-size: 1.4rem;
+        color: #aaa;
     }
 `;
