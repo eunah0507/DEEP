4da885b189ac58399dc9d9317427cf05b45cda78
WHOOZ-23 <riahboa@naver.com>
2024-05-18 21:06:19 +0900

    :sparkles:feat: 에러 페이지 구현 완료

--- FE/deep/src/pages/NotFoundPage/NotFoundPage.js
+++ FE/deep/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,5 +1,24 @@
+import { NotFoundContainer, NotFoundWrapper } from "./NotFoundPage.styles";
+import deepLogo from "../../assets/images/deep-icon.png";
+
 function NotFoundPage() {
-    return <div>404 Not Found</div>;
+    return (
+        <NotFoundWrapper>
+            <NotFoundContainer>
+                <div className="deep_icon">
+                    <img src={deepLogo} alt="deep icon" />
+                </div>
+                <div className="text_container">
+                    <h3>앗! 페이지를 찾을 수가 없어요...</h3>
+                    <p>
+                        네트워크가 일시적으로 끊겼거나 페이지를 불러오지
+                        못했습니다.
+                    </p>
+                    <p>잠시 후 다시 시도해 주세요.</p>
+                </div>
+            </NotFoundContainer>
+        </NotFoundWrapper>
+    );
 }
 
 export default NotFoundPage;
